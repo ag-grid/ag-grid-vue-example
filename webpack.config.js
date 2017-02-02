82b0d9a4f4339f18830f51009c53ba9286caebc3
Sean Landsman <sean@thelandsmans.com>
2017-02-02 16:18:37 +0000

    AG-205 Finish examples. Add routing and styling.

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
-                test: /\.(png|jpg|gif|svg)$/,
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
                 loader: 'file-loader',
                 options: {
                     name: '[name].[ext]?[hash]'
