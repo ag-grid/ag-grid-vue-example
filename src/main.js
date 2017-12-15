b108e8aff985b15811448e11cfb53095d36c593c
Sean Landsman <sean@thelandsmans.com>
2017-12-15 14:59:09 +0000

    AG-1294 Sweep framework examples & update versions Sprint IV

--- src/main.js
+++ src/main.js
@@ -11,27 +11,19 @@ import "ag-grid-enterprise/main";
 // import {LicenseManager} from "ag-grid-enterprise/main";
 // LicenseManager.setLicenseKey("your license key");
 
-import RichGridExample from "./rich-grid-example/RichGridExample.vue";
 import DynamicComponentExample from "./dynamic-component-example/DynamicComponentExample.vue";
-import RichDynamicComponentExample from "./rich-dynamic-component-example/RichDynamicComponentExample.vue";
-import EditorComponentExample from "./editor-component-example/EditorComponentExample.vue";
 import FloatingRowExample from "./floating-row-example/FloatingRowExample.vue";
 import FullWidthRowExample from "./full-width-example/FullWidthExample.vue";
 import GroupedRowExample from "./grouped-row-example/GroupedRowExample.vue";
-import FilterExample from "./filter-example/FilterExample.vue";
 import LargeDataSetExample from "./large-data-set-example/LargeDataSetExample.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-    {path: '/', component: RichGridExample, name: "Rich Grid with Pure JavaScript"},
-    {path: '/dynamic', component: DynamicComponentExample, name: "Dynamic Components Example"},
-    {path: '/rich-dynamic', component: RichDynamicComponentExample, name: "Dynamic Components - Richer Example"},
-    {path: '/editor', component: EditorComponentExample, name: "Editor Component Example"},
+    {path: '/', component: DynamicComponentExample, name: "Rich Grid with Pure JavaScript"},
     {path: '/floating', component: FloatingRowExample, name: "Pinned Row Example"},
     {path: '/full-width', component: FullWidthRowExample, name: "Full Width Row Example"},
     {path: '/grouped-row', component: GroupedRowExample, name: "Grouped Row Example"},
-    {path: '/filter', component: FilterExample, name: "Filter Example"},
     {path: '/large-data', component: LargeDataSetExample, name: "Large Data Example"}
 ];
 
