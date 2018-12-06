a54380d83fa9f2b5e6240ad5fedeafb5da1f069c
Sean Landsman <sean@thelandsmans.com>
2018-12-06 13:54:12 +0000

    AG-2272 Improve Vue Dev Experience

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    // css: {
-    //     loaderOptions: {
-    //         postcss: {
-    //             config: {
-    //                 path: './postcss.config.js'
-    //             }
-    //         }
-    //     }
-    // },
     configureWebpack: {
         resolve: {
+            symlinks: false,
             alias: {
                 // Make sure *our* version of ag-grid & vue is always loaded.
                 // This is needed for `yarn link / npm link` to work and prevent duplicate versions of these libs
                 // being loaded
-                'ag-grid-community/main': path.resolve(__dirname , 'node_modules/ag-grid-community/main.js'),
-                'ag-grid-community$': path.resolve(__dirname , 'node_modules/ag-grid-community/main.js'),
-                vue$: path.resolve(__dirname , 'node_modules/vue/dist/vue.common.js')
+                'ag-grid-community/main': path.resolve(__dirname, 'node_modules/ag-grid-community/main.js'),
+                'ag-grid-community$': path.resolve(__dirname, 'node_modules/ag-grid-community/main.js'),
+                vue$: path.resolve(__dirname, 'node_modules/vue/dist/vue.common.js')
             }
         },
-        performance : {
-            hints : false
+        performance: {
+            hints: false
         }
     }
 };
