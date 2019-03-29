8225f1f8cffe8fba93d8077eb01a3b61b7a94448
Sean Landsman <sean@thelandsmans.com>
2019-03-29 16:09:00 +0000

    AG-2257 Update and expand tests documentation/guide

---
+++ tests/GridExample.vue
@@ -0,0 +1,55 @@
+<template>
+    <ag-grid-vue style="width: 500px; height: 500px;"
+                 class="ag-theme-balham"
+                 @grid-ready="onGridReady"
+                 :columnDefs="columnDefs"
+                 :rowData="rowData">
+    </ag-grid-vue>
+</template>
+
+<script>
+    import {AgGridVue} from "ag-grid-vue";
+    import Renderer from './Renderer.vue';
+    import Editor from './Editor.vue';
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                columnDefs: null,
+                rowData: null,
+                api: null
+            }
+        },
+        components: {
+            AgGridVue,
+            Renderer,
+            Editor
+        },
+        beforeMount() {
+            this.columnDefs = [
+                {field: 'make'},
+                {
+                    field: 'price',
+                    editable: true,
+                    cellRendererFramework: 'Renderer',
+                    cellEditorFramework: 'Editor'
+                }
+            ];
+
+            this.rowData = [
+                {make: 'Toyota', price: '35000'},
+            ];
+        },
+        methods: {
+            onGridReady(params) {
+                this.api = params.api;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
+
+
